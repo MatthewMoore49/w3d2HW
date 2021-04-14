1ed17b98d9ed0168bf7f68fed2bdaf11c81864af
Matt <msm3390@gmail.com>
2021-04-13 23:01:16 -0400

    homework

---
+++ app.js
@@ -0,0 +1,49 @@
+
+var a = 5;
+var b = 7;
+function addSum(a,b){
+totalSum = a + b;
+console.log(totalSum);
+
+}
+
+
+
+
+
+
+
+
+//For today's homework, please complete the following steps.
+
+//1. Create a project with an index.html file and link an app.js file to it.
+
+
+//2. Create two variables that have numbers as their values
+var a = "5";
+var b = "7";
+
+//3. Create a function that returns the sum of two numbers that are arguments. Then console.log the function with the variables from step two as your two arguments.
+function addSum(a,b){
+    answer = a + b;
+//4. Create a function that returns the difference of two numbers that are arguments. Then console.log the function with the variables from step two as your two arguments.
+
+    console.log (answer);
+}
+
+//5. Create a function that returns two arguments multiplied by each other. Then console.log the function with the variables from step two as your two arguments.
+function multipleNos(a,b){
+    answer = a * b;
+    console.log (answer);
+}
+//6. Create a function that returns divides the first argument by the second argument and returns the new value. Then console.log the function with the variables from step two as your two arguments.
+var x = 20
+var y = 5
+function divNos(x,y){
+answer = a / b;
+console.log (answer);
+
+}
+
+
+//7. Once you are done, deploy your project on GitHub and submit a link to your repository.
